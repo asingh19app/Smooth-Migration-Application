262b6fdfe7007b7c738b0fbabbd3b8c2a93c6f00
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2025-03-08 16:45:59 -0500

    Initial Commit

---
+++ Smooth-Migration-Application/Smooth_Migration_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smooth_Migration_ApplicationApp.swift
+//  Smooth-Migration-Application
+//
+//  Created by Adeshvir Dhanoa on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Smooth_Migration_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smooth-Migration-ApplicationTests/Smooth_Migration_ApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smooth_Migration_ApplicationTests.swift
+//  Smooth-Migration-ApplicationTests
+//
+//  Created by Adeshvir Dhanoa on 3/8/25.
+//
+
+import Testing
+@testable import Smooth_Migration_Application
+
+struct Smooth_Migration_ApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
